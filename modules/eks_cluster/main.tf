21761b1b3e1a5b72b81cbc691d3c22cc33472a54
PrithwishMandalGDS <prithwishm969@gmail.com>
2024-12-04 13:05:13 +0000

    naming convention issue fix

--- modules/eks_cluster/main.tf
+++ modules/eks_cluster/main.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_cluster" "cluster" {
   name     = "eks-cross-plane-pov"
-  role_arn = aws_iam_role.eks_service_role_pov_pov_pov_pov_pov_pov.arn
+  role_arn = aws_iam_role.eks_service_role_pov.arn
 
   vpc_config {
     subnet_ids = var.subnet_array_value
